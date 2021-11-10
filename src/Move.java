b9acaee1b3d17803877284d2ff777dec6313654c
Marie <S185363@student.dtu.dk>
2021-11-10 20:56:38 +0100

    Oprettet branch. Lavet convertIndex i move klassen. Mangler test

--- src/Move.java
+++ src/Move.java
@@ -21,9 +21,51 @@ public class Move {
     public void pathChecker(){}
 
     // TODO: write this function
-    public void convertIndexTo2D(int location){
-        // 0,1,2,3,4,5,6,7
-        // 8,9,10,11,12,13,14,15
+    public int[] convertIndexTo2D(int location){
+        int[] array = new int[2];
+
+        //Get the x-coordinate
+    if (0 <= location && location <= 7)                       array[1] = 0;
+        else if (8 <= location && location <= 15)             array[1] = 1;
+        else if (16 <= location && location <= 23)            array[1] = 2;
+        else if (24 <= location && location <= 31)            array[1] = 3;
+        else if (32 <= location && location <= 39)            array[1] = 4;
+        else if (40 <= location && location <= 47)            array[1] = 5;
+        else if (48 <= location && location <= 55)            array[1] = 6;
+        else if (56 <= location && location <= 63)            array[1] = 7;
+
+
+        switch (location % 8){ //Get the y-coordinate
+            case 0:
+                array[0] = 0;
+                break;
+            case 1:
+                array[0] = 1;
+                break;
+            case 2:
+                array[0] = 2;
+                break;
+            case 3:
+                array[0] = 3;
+                break;
+            case 4:
+                array[0] = 4;
+                break;
+            case 5:
+                array[0] = 5;
+                break;
+            case 6:
+                array[0] = 6;
+                break;
+            case 7:
+                array[0] = 7;
+                break;
+
+        }
+        return array;
+
+        //  0, 1, 2, 3, 4, 5, 6, 7
+        //  8, 9,10,11,12,13,14,15
         // 16,17,18,19,20,21,22,23
         // 24,25,26,27,28,29,30,31
         // 32,33,34,35,36,37,38,39
@@ -55,6 +97,7 @@ public class Move {
                 break;
             case 'q':
             case 'Q':
+                queenMoves(location);
                 System.out.println("");
                 break;
             case 'r':
@@ -70,6 +113,12 @@ public class Move {
                 System.out.println("");
                 break;
         }
+    }
+
+    public void queenMoves(int location){
+
+
+
     }
 
 
