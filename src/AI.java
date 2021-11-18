7ee05d8f7c9ffe2f7ad49632bb0156ed1a48b633
Marie <S185363@student.dtu.dk>
2021-11-18 14:09:37 +0100

    No more static

--- src/AI.java
+++ src/AI.java
@@ -14,7 +14,7 @@ public class AI {
     public void runAI() {
     }
 
-    public static int minimax(ChessNode nodeToSearch, int depth, boolean isMax, int alpha, int beta) {
+    public int minimax(ChessNode nodeToSearch, int depth, boolean isMax, int alpha, int beta) {
 
         System.out.println("Depth in minimax " + depth); //todo delete after use
 
@@ -33,7 +33,7 @@ public class AI {
         return 0;
     }
 
-    private static int maximizer(ChessNode nodeToSearch, int depth, int alpha, int beta) {
+    private int maximizer(ChessNode nodeToSearch, int depth, int alpha, int beta) {
         int bestValue = alpha;
         int value;
         System.out.println("Max alpha " + alpha); //todo debug print
@@ -52,7 +52,7 @@ public class AI {
         return bestValue;
     }
 
-    private static int minimizer(ChessNode nodeTosearch, int depth, int alpha, int beta) {
+    private int minimizer(ChessNode nodeTosearch, int depth, int alpha, int beta) {
         int bestValue = beta;
         int value;
         System.out.println("Min alpha " + alpha);
@@ -72,7 +72,7 @@ public class AI {
     }
 
     // - - - - - Evaluate - - - - - //todo exapnd these
-    public static int evaluateLeaf() { // so far based on slide 11 in pptx week 6.
+    public int evaluateLeaf() { // so far based on slide 11 in pptx week 6.
         int sum = 0;
 
         sum += evaluatePawn(0, 0);
@@ -86,13 +86,13 @@ public class AI {
 
     // todo should differentiate between min/max, white/black
     // Takes position as argument and performs lookup of position value in value table
-    private static int evaluatePawn(int i, int j) {
+    private int evaluatePawn(int i, int j) {
         return 100 + BoardEvaluationData.getWhitePawnValue(i, j);
     }
 
 
 
-    public static void fillChildren(ChessNode parent){
+    public void fillChildren(ChessNode parent){
 
         // get parrent board
         char[][] boardParent = parent.getBoard().getBoardArray();
@@ -114,34 +114,34 @@ public class AI {
         }
     }
 
-    private static int evaluateRook(int i, int j) {
+    private int evaluateRook(int i, int j) {
         return 500 + BoardEvaluationData.getRookValue(i, j);
     }
 
-    private static int evaluateBishop(int i, int j) {
+    private int evaluateBishop(int i, int j) {
         return 300 + BoardEvaluationData.getBishopValue(i, j);
     }
 
-    private static int evaluateKnight(int i, int j) {
+    private int evaluateKnight(int i, int j) {
         return 300 + BoardEvaluationData.getKnightValue(i, j);
     }
 
-    private static int evaluateQueen(int i, int j) {
+    private int evaluateQueen(int i, int j) {
         return 900 + BoardEvaluationData.getQueenValue(i, j);
     }
 
-    private static int evaluateKing(int i, int j) {
+    private int evaluateKing(int i, int j) {
         return 10000 + BoardEvaluationData.getKingValue(i, j);
     }
 
 //- - - - - - - - - - - - - - - - - - - - //
 
-    public static int max(int a, int b) {
+    public int max(int a, int b) {
         if (a > b) return a;
         else return b;
     }
 
-    public static int min(int a, int b) {
+    public int min(int a, int b) {
         if (a < b) return a;
         else return b;
     }
