d68ed24c69d393d82859f7f7ccd770f9bc0bb8a6
Repeller <29373163+Repeller@users.noreply.github.com>
2021-11-22 15:13:44 +0100

    main is now back to normal

--- src/Main.java
+++ src/Main.java
@@ -18,137 +18,137 @@ public class Main {
 
     public static void main(String[] args) {
 
-        testMovesetFunctions();
-//        tui = new TUI();
-//        sc = new Scanner(System.in);
-//        int gameModeSelection;
-//        Fen resumeFen;
-//        char startPiece = ' ';
-//        char destinationPiece = ' ';
-//        Player player;
-//        turn = true;
-//
-//        // Prompt for resume game
-//        resumeFen = tui.showResumeMenu(sc);
-//
-//        // Prompt for gamemode
-//        gameModeSelection = tui.showStartMenu(sc);
-//
-//        switch (gameModeSelection) {
-//            case 1 -> { // Human vs AI
-//                p1 = new Player(true, false);
-//                p2 = new Player(false, true);
-//            }
-//            case 2 -> { // Human vs Human
-//                p1 = new Player(true, false);
-//                p2 = new Player(false, false);
-//            }
-//            case 3 -> { // AI vs AI
-//                p1 = new Player(true, true);
-//                p2 = new Player(false, true);
-//            }
-//            default -> System.out.println("Error in game mode selection: " + gameModeSelection);
-//        }
-//
-//        board = new Board();
-//        game = new Game(board, p1, p2, turn);
-//
-//        // Resume game
-//        if (resumeFen != null) {
-//            turn = resumeFen.getPlayerTurn();
-//            board.setBoard(resumeFen.getBoardLayout());
-//            game.setTurn(turn);
-//            game.setTurnsSinceKill(resumeFen.getTurnsSinceKill());
-//            game.setTotalTurns(resumeFen.getTotalTurns());
-//            game.setCastling(resumeFen.getCastling());
-//            game.setEnPassantTarget(resumeFen.getEnPassantTarget());
-//            tui.showResumeGameData(resumeFen);
-//        }
-//
-//        //tui.initBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"); // Using default test FEN
-//
-//        /* Start the main game loop */
-//        while (true) {
-//
-//            // todo check end game conditions (checkmate, 50 moves no kill etc.)
-//            if (game.turnsSinceKill >= 50) {
-//                System.out.println("********** GAME OVER: Draw **********");
-//                break;
-//            }
-//
-//            player = game.getPlayerTurn(turn);
-//
-//            if (player.isWhite()) {
-//                System.out.println("White Players turn");
-//            } else {
-//                System.out.println("Black Players turn");
-//                // Increment after each black player move.
-//                game.setTotalTurns(game.getTotalTurns()+1);
-//            }
-//
-//            tui.printBoard(board.getBoardArray(), !turn);
-//
-//            if (!player.isAi) { // Human player's turn
-//
-//                int[] movePos;
-//
-//                // Get start position and destination
-//                while (true) {
-//
-//                    System.out.println("Please enter your move ex. (a2 a3) ");
-//                    movePos = tui.getMovePosition(sc);
-//
-//                    // Validate piece based on user input
-//                    try {
-//                        startPiece = board.checkStartPosition(player.isWhite(), movePos[0], movePos[1]);
-//                    } catch (Exception e) {
-//                        System.out.println("Error: Invalid piece");
-//                    }
-//
-//                    try {
-//                        destinationPiece = board.getPiece(movePos[2], movePos[3]);
-//                    } catch (Exception e) {
-//                        System.out.println("Error: Invalid destination selected!");
-//                    }
-//
-//                    /* Check if chosen board position is empty */
-//                    if (startPiece != ' ') {
-//
-//                        boolean isKill = board.isEnemyPiece(player.isWhite(), destinationPiece);
-//
-//                        /* Check if chosen destination position is empty or enemy */
-//                        if (destinationPiece == ' ' || isKill) {
-//                            Move move = new Move(new int[]{movePos[2], movePos[3]}, new int[]{movePos[0], movePos[1]}, false, startPiece, ' ');
-//                            board.performMove(move);
-//
-//                            if (isKill) {
-//                                /* Reset kill counter */
-//                                game.setTurnsSinceKill(0);
-//                            } else {
-//                                game.setTurnsSinceKill(game.getTurnsSinceKill() + 1);
-//                            }
-//
-//                            System.out.println("Move complete!");
-//                            System.out.println("------------------------");
-//                            turn = !turn;
-//
-//                            // Next player's turn
-//                            break;
-//                        }
-//                    }
-//
-//                }
-//
-//            } else { // AI player's turn
-//                // todo finish AI player
-//                tui.printBoard(board.getBoardArray(), false);
-//                turn = !turn;
-//            }
-//        }
-//
-//        // todo check if game is over?
-//        //System.out.println(" "+(turn ? "Black" : "White" + " player has won the game"));
-//        //sc.close();
+//        testMovesetFunctions();
+        tui = new TUI();
+        sc = new Scanner(System.in);
+        int gameModeSelection;
+        Fen resumeFen;
+        char startPiece = ' ';
+        char destinationPiece = ' ';
+        Player player;
+        turn = true;
+
+        // Prompt for resume game
+        resumeFen = tui.showResumeMenu(sc);
+
+        // Prompt for gamemode
+        gameModeSelection = tui.showStartMenu(sc);
+
+        switch (gameModeSelection) {
+            case 1 -> { // Human vs AI
+                p1 = new Player(true, false);
+                p2 = new Player(false, true);
+            }
+            case 2 -> { // Human vs Human
+                p1 = new Player(true, false);
+                p2 = new Player(false, false);
+            }
+            case 3 -> { // AI vs AI
+                p1 = new Player(true, true);
+                p2 = new Player(false, true);
+            }
+            default -> System.out.println("Error in game mode selection: " + gameModeSelection);
+        }
+
+        board = new Board();
+        game = new Game(board, p1, p2, turn);
+
+        // Resume game
+        if (resumeFen != null) {
+            turn = resumeFen.getPlayerTurn();
+            board.setBoard(resumeFen.getBoardLayout());
+            game.setTurn(turn);
+            game.setTurnsSinceKill(resumeFen.getTurnsSinceKill());
+            game.setTotalTurns(resumeFen.getTotalTurns());
+            game.setCastling(resumeFen.getCastling());
+            game.setEnPassantTarget(resumeFen.getEnPassantTarget());
+            tui.showResumeGameData(resumeFen);
+        }
+
+        //tui.initBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"); // Using default test FEN
+
+        /* Start the main game loop */
+        while (true) {
+
+            // todo check end game conditions (checkmate, 50 moves no kill etc.)
+            if (game.turnsSinceKill >= 50) {
+                System.out.println("********** GAME OVER: Draw **********");
+                break;
+            }
+
+            player = game.getPlayerTurn(turn);
+
+            if (player.isWhite()) {
+                System.out.println("White Players turn");
+            } else {
+                System.out.println("Black Players turn");
+                // Increment after each black player move.
+                game.setTotalTurns(game.getTotalTurns()+1);
+            }
+
+            tui.printBoard(board.getBoardArray(), !turn);
+
+            if (!player.isAi) { // Human player's turn
+
+                int[] movePos;
+
+                // Get start position and destination
+                while (true) {
+
+                    System.out.println("Please enter your move ex. (a2 a3) ");
+                    movePos = tui.getMovePosition(sc);
+
+                    // Validate piece based on user input
+                    try {
+                        startPiece = board.checkStartPosition(player.isWhite(), movePos[0], movePos[1]);
+                    } catch (Exception e) {
+                        System.out.println("Error: Invalid piece");
+                    }
+
+                    try {
+                        destinationPiece = board.getPiece(movePos[2], movePos[3]);
+                    } catch (Exception e) {
+                        System.out.println("Error: Invalid destination selected!");
+                    }
+
+                    /* Check if chosen board position is empty */
+                    if (startPiece != ' ') {
+
+                        boolean isKill = board.isEnemyPiece(player.isWhite(), destinationPiece);
+
+                        /* Check if chosen destination position is empty or enemy */
+                        if (destinationPiece == ' ' || isKill) {
+                            Move move = new Move(new int[]{movePos[2], movePos[3]}, new int[]{movePos[0], movePos[1]}, false, startPiece, ' ');
+                            board.performMove(move);
+
+                            if (isKill) {
+                                /* Reset kill counter */
+                                game.setTurnsSinceKill(0);
+                            } else {
+                                game.setTurnsSinceKill(game.getTurnsSinceKill() + 1);
+                            }
+
+                            System.out.println("Move complete!");
+                            System.out.println("------------------------");
+                            turn = !turn;
+
+                            // Next player's turn
+                            break;
+                        }
+                    }
+
+                }
+
+            } else { // AI player's turn
+                // todo finish AI player
+                tui.printBoard(board.getBoardArray(), false);
+                turn = !turn;
+            }
+        }
+
+        // todo check if game is over?
+        //System.out.println(" "+(turn ? "Black" : "White" + " player has won the game"));
+        //sc.close();
     }
 
     public static void arrayPrinter(ArrayList<int[]> input) {
