a5b657e758f7055f10b51fb58cd2e8b049348aa3
Darthlatte <savethecat@gmail.com>
2021-11-30 00:03:00 +0100

    Logging of moves to file (txt)

---
+++ src/Helper/Writer.java
@@ -0,0 +1,40 @@
+package Helper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Writer {
+
+    private FileWriter fw;
+    private BufferedWriter output;
+    private String fileName = "./src/Data/Moves.txt";
+    private Path filePath = Path.of(fileName);
+
+    public void initialize() {
+        try {
+            if (!Files.exists(filePath)) {
+                Files.createFile(Path.of(fileName));
+            }
+            fw = new FileWriter(fileName,false);
+            output = new BufferedWriter(fw);
+            output.write("************* Game Begin *************\n");
+            output.close();
+        } catch (IOException IOE) {
+            System.out.println("FileWriter Error in initialize(): " + IOE);
+        }
+    }
+
+    public void write(String s) {
+        try {
+            fw = new FileWriter(fileName,true);
+            output = new BufferedWriter(fw);
+            output.write(s+"\n");
+            output.close();
+        } catch (IOException IOE) {
+            System.out.println("FileWriter Error in write(): " + IOE);
+        }
+    }
+}
